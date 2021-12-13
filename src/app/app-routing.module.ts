bee95946c954491a7701b0b875638178ac64e4dc
juancguerrero <jcamiloguerrerog@gmail.com>
2021-12-12 20:26:27 -0500

    add module home and component home, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import ('./login/login.module').then(m => m.LoginModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import ('./home/home.module').then(m => m.HomeModule)
+  }
 ];
 
 @NgModule({
